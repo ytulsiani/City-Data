45d1138d41db5ef02daa430b803b7e7255f820d5
Mahati <mvavilala3@gatech.edu>
2017-04-23 03:25:11 -0400

    POI Detail Screen Done

--- src/main/java/controller/POIDetailController.java
+++ src/main/java/controller/POIDetailController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import fxapp.MainFXApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 /**
  * Created by Mahati on 4/23/2017.
@@ -10,4 +12,45 @@ public class POIDetailController {
     public void register(MainFXApp main) {
         this.main = main;
     }
+
+    @FXML
+    private ComboBox type;
+    @FXML
+    private TextField dataValue1;
+    @FXML
+    private TextField dataValue2;
+    @FXML
+    private DatePicker date1;
+    @FXML
+    private DatePicker date2;
+    @FXML
+    private Button applyFilter;
+    @FXML
+    private Button resetFilter;
+
+    @FXML
+    private TableView poiDetailTable;
+    @FXML
+    private Button back;
+    @FXML
+    private Button flag;
+
+
+    @FXML
+    public void onApplyFilterClick() {
+
+    }
+    @FXML
+    public void onResetFilterClick() {
+
+    }
+    @FXML
+    public void onBackClick() {
+        main.setMainScene(MainFXApp.userType, MainFXApp.user);
+    }
+
+    @FXML
+    public void onFlagClick() {
+    }
+
 }
