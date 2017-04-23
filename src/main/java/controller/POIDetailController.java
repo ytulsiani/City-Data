0649ea8efcf338ef68f920511e0a6271e2e19d6e
Mahati <mvavilala3@gatech.edu>
2017-04-23 03:07:19 -0400

    POI Report Screen Done

---
+++ src/main/java/controller/POIDetailController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import fxapp.MainFXApp;
+
+/**
+ * Created by Mahati on 4/23/2017.
+ */
+public class POIDetailController {
+    private MainFXApp main;
+    public void register(MainFXApp main) {
+        this.main = main;
+    }
+}
--- src/main/java/controller/POIReportController.java
+++ src/main/java/controller/POIReportController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import fxapp.MainFXApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 /**
  * Created by Yash on 4/23/2017.
@@ -9,5 +12,15 @@ public class POIReportController {
     private MainFXApp main;
     public void register(MainFXApp main) {
         this.main = main;
+    }
+
+    @FXML
+    private TableView poiReportTable;
+    @FXML
+    private Button back;
+
+    @FXML
+    public void onBackClick() {
+        main.setMainScene(MainFXApp.userType, MainFXApp.user);
     }
 }
