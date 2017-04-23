7f06503b6eef7e62c1c9446de24208abcb8270f0
Mudge, Zachary Bryant <zmudge@gatech.edu>
2017-04-22 22:54:38 -0400

    DBConnection.connectAndUpdate

--- src/main/java/fxapp/DBConnection.java
+++ src/main/java/fxapp/DBConnection.java
@@ -8,40 +8,32 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 
 public class DBConnection {
-    public static ResultSet  connectAndQuery(Statement stmt, String query) throws SQLException {
+    public static ResultSet connectAndQuery(Statement stmt, String query) throws SQLException {
         Connection con = null;
-
-            //Class.forName("com.mysql.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_86", "cs4400_86", "pAtSRI1i");
-            ResultSet returnSet= null;
-            if (!con.isClosed()) {
-                System.out.println("Successfully connected to MySQL server");
-            }
-            //Statement stmt = null;
-            //CHANGE SO IT WORKS WITH QUERYING THE DATABSE
-            try {
-                stmt = con.createStatement();
-                returnSet = stmt.executeQuery(query);
-            } catch(SQLException e) {
-                System.out.println(e);
-            } finally {
-
-            }
-
-            //sql test
-
-
-
-            /*while (blah.next()) {
-                String str = blah.getString("EmailAddress");
-                if (str == null) {
-                    System.out.println("blah");
-                }
-                System.out.println(str);
-            }*/
+        con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_86", "cs4400_86", "pAtSRI1i");
+        ResultSet returnSet = null;
+        if (!con.isClosed()) {
+            System.out.println("Successfully connected to MySQL server");
+        }
+        try {
+            stmt = con.createStatement();
+            returnSet = stmt.executeQuery(query);
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
         return returnSet;
     }
-    public static void connectAndExecute(String query) {
-
+    public static void connectAndUpdate(Statement stmt, String query) throws SQLException {
+        Connection con = null;
+        con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_86", "cs4400_86", "pAtSRI1i");
+        if (!con.isClosed()) {
+            System.out.println("Successfully connected to MySQL server");
+        }
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate(query);
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
     }
 }
