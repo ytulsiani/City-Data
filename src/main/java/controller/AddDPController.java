c1afc250b62525d9efabab23205ad8549ebe1798
Mahati <mvavilala3@gatech.edu>
2017-04-23 02:11:16 -0400

    AddDP Done

--- src/main/java/controller/AddDPController.java
+++ src/main/java/controller/AddDPController.java
@@ -1,6 +1,11 @@
 package controller;
 
 import fxapp.MainFXApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import sun.applet.Main;
 
 /**
@@ -10,5 +15,30 @@ public class AddDPController {
     private MainFXApp main;
     public void register(MainFXApp main) {
         this.main = main;
+    }
+
+    @FXML
+    private ComboBox poiLocationName;
+    @FXML
+    private DatePicker timeDataReading;
+    @FXML
+    private ComboBox dataType;
+    @FXML
+    private TextField dataValue;
+    @FXML
+    private Button back;
+    @FXML
+    private Button submit;
+
+    @FXML
+    public void onAddLocationClick() {
+    }
+
+    @FXML
+    public void onBackClick() {
+    }
+
+    @FXML
+    public void onSubmitClick() {
     }
 }
