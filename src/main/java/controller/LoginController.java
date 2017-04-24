05b1a44f350a488336c37060cd51772b7363c44f
Mudge, Zachary Bryant <zmudge@gatech.edu>
2017-04-23 23:17:49 -0400

    City official approval works

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -37,14 +37,29 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
         Statement stmt = null;
+        Statement stmt2 = null;
         String query = "SELECT Username, Password, UserType FROM cs4400_86.USER WHERE Username = '" + username +
                 "' AND Password = '" + password + "'";
         try {
             ResultSet result = DBConnection.connectAndQuery(stmt, query);
+
             boolean correct = false;
             while (result.next()) {
-                correct = true;
-                main.setMainScene(result.getString("UserType"), result.getString("Username"));
+                if (result.getString("UserType").equals("City Official")) {
+                    String query2 = "SELECT Approved FROM CITY_OFFICIAL WHERE Username = '" + username + "'";
+                    ResultSet result2 = DBConnection.connectAndQuery(stmt2, query2);
+                    while (result2.next()) {
+                        correct = true;
+                        if (result2.getString("Approved") == null || result2.getString("Approved").equals("0")) {
+                            errorText.setText("Your city official account hasn't been approved yet!");
+                        } else {
+                            main.setMainScene(result.getString("UserType"), result.getString("Username"));
+                        }
+                    }
+                } else {
+                    correct = true;
+                    main.setMainScene(result.getString("UserType"), result.getString("Username"));
+                }
             }
             if (correct == false) {
                 errorText.setText("Incorrect username or password!");
@@ -54,6 +69,9 @@ public class LoginController {
         } finally {
             if (stmt != null) {
                 stmt.close();
+            }
+            if (stmt2 != null) {
+                stmt2.close();
             }
         }
 
